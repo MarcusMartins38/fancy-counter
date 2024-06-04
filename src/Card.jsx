22b9c7d98659e67c05a6e290db753a6a62cc8d04
unknown <marcusmartins38@gmail.com>
2024-06-03 22:08:31 -0300

    chore: useEffect with keydown

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Count from "./Count";
 import ButtonContainer from "./ButtonContainer";
 import ResetButton from "./ResetButton";
@@ -6,13 +6,28 @@ import Title from "./Title";
 
 export default function Card() {
   const [count, setCount] = useState(0);
+  const locked = count === 5 ? true : false;
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      if (event.code === "Space") {
+        setCount(count + 1);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, [count]);
 
   return (
-    <div className="card">
-      <Title />
+    <div className={`card ${locked ? "card--limit" : ""}`}>
+      <Title locked={locked} />
       <Count count={count} />
       <ResetButton setCount={setCount} />
-      <ButtonContainer setCount={setCount} />
+      <ButtonContainer setCount={setCount} locked={locked} />
     </div>
   );
 }
